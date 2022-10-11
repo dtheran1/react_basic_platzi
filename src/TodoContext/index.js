fa2b190862b1c0a50de0dbbe2ad8c9cb8cf5a86f
Dtheran <dtheran1@gmail.com>
2022-10-10 21:28:49 -0500

    wip

---
+++ src/TodoContext/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+const TodoContext = React.createContext()
+// const { Provider, Consumer } = createContext();
+
+function TodoProvider(props) {
+    const { item: todos, saveItem: saveTodos, loading, error } = useLocalStorage('TODOS_V1', []);
+    const [search, setSearch] = useState('')
+    const completedTodos = todos.filter(item => !!item.completed).length;
+    const totalTodos = todos.length;
+
+    let searchedTodos = []
+    if (!search >= 1) {
+        searchedTodos = todos
+    } else {
+        searchedTodos = todos.filter(item => (
+            item.text.toLowerCase().includes(search.toLowerCase())
+        ))
+    };
+
+    const onCompletedTodo = (text) => {
+        const indItem = todos.findIndex(e => e.text === text);
+        const newTodos = [...todos];
+        newTodos[indItem].completed = !newTodos[indItem].completed;
+        saveTodos(newTodos);
+    }
+
+    const onDeletedTodo = (text) => {
+        const indItem = todos.findIndex(e => e.text === text);
+        const newTodos = [...todos];
+        newTodos.splice(indItem, 1);
+        saveTodos(newTodos);
+    }
+
+    return (
+        <TodoContext.Provider value={{
+            completedTodos,
+            totalTodos,
+            search,
+            setSearch,
+            searchedTodos,
+            onCompletedTodo,
+            onDeletedTodo,
+            loading,
+            error,
+        }}>
+            {props.children}
+        </TodoContext.Provider>
+    );
+}
+
+export { TodoProvider, TodoContext }
---
+++ src/TodoContext/useLocalStorage.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+function useLocalStorage(itemName, initialValue) {
+    const [item, setItem] = useState(initialValue);
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => {
+            try {
+                const localStorageItem = localStorage.getItem(itemName)
+                let parsedItem;
+                if (!localStorageItem) {
+                    localStorage.setItem(itemName, JSON.stringify(initialValue))
+                    parsedItem = initialValue;
+                } else {
+                    parsedItem = JSON.parse(localStorageItem);
+                }
+                setItem(parsedItem)
+                setLoading(false)
+            } catch (error) {
+                setError(error)
+            }
+        }, 3000);
+    })
+
+    const saveItem = (newItem) => {
+        try {
+            localStorage.setItem(itemName, JSON.stringify(newItem));
+            setItem(newItem);
+        } catch (error) {
+            setError(error)
+        }
+    }
+
+    return { item, saveItem, loading, error }
+}
+
+export { useLocalStorage }
