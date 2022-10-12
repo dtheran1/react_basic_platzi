403edb4fd228a2a1ec57d7e66474f37347946ff2
Dtheran <dtheran1@gmail.com>
2022-10-12 10:01:23 -0500

    add icons

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { CompleteIcon } from "../TodoIcon/CompleteICon";
+import { DeleteIcon } from "../TodoIcon/DeleteIcon";
 import './TodoItem.css'
 export default function TodoItem({ text, completed, onComplete, onDelete }) {
 
     return (
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${completed && 'Icon-check--active'}`} onClick={onComplete}> √</span>
+            <CompleteIcon completed={completed} onComplete={onComplete} />
             <p className={`TodoItem-p ${completed && 'TodoItem-p--complete'}`}>{text}</p>
-            <span className="Icon Icon-delete" onClick={onDelete}>X</span>
+            <DeleteIcon onDelete={onDelete} />
         </li>
     )
 }
